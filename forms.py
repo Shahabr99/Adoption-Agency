f6709f942f74b02700a0ea0dd101b5a70e12efb6
Shahabr99 <sean.rohani9@gmail.com>
2023-05-06 22:58:23 -0700

    added more validators

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SelectField
-from wtforms.validators import InputRequired, Email, Optional
+from wtforms.validators import InputRequired, NumberRange, Optional, URL, AnyOf
 
 
 class PetAdoptionForm(FlaskForm):
     """Create adoption form"""
 
     name=StringField('Pet name', validators= [InputRequired()])
-    species = StringField('Species', validators=[Optional()])
-    photo = StringField('Photo URL')
-    age = IntegerField('Age', validators=[InputRequired()])
+    species = StringField('Species', validators=[Optional(), AnyOf(values=['cat', 'dog', 'porcupine', 'Cat', 'Dog', 'Porcupine'], message="Invalid species")])
+    photo = StringField('Photo URL', validators=[URL(require_tld=True)])
+    age = IntegerField('Age', validators=[InputRequired(), NumberRange(min=1, max=30)])
     notes = StringField('Notes', validators=[Optional()])
